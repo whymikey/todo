ddc8dcb6fc1d565f443a12a3dada04fcb5090fb4
mikey <mikushawasted@gmail.com>
2026-03-02 06:16:04 +0300

    added filter

---
+++ src/components/scripts/filter.js
@@ -0,0 +1,39 @@
+function filterTasks({ tasksList, tasks }, filterType) {
+  const tasksListElements = tasksList.querySelectorAll(".todo_task_element");
+
+  tasksListElements.forEach((el) => {
+    const taskId = +el.dataset.id;
+    const task = tasks.find((task) => task.id === taskId);
+
+    if (!task) return;
+
+    switch (filterType) {
+      case "completed":
+        el.style.display = task.status ? "" : "none";
+        break;
+
+      case "active":
+        el.style.display = !task.status ? "" : "none";
+        break;
+
+      case "all":
+      default:
+        el.style.display = "";
+        break;
+    }
+  });
+}
+
+export function getProgress(evt, { tasksList, tasks }) {
+  const progressElement = evt.target;
+
+  if (progressElement.classList.contains("todo_progress_text--all")) {
+    filterTasks({ tasksList, tasks }, "all");
+  } else if (
+    progressElement.classList.contains("todo_progress_text--completed")
+  ) {
+    filterTasks({ tasksList, tasks }, "completed");
+  } else if (progressElement.classList.contains("todo_progress_text--active")) {
+    filterTasks({ tasksList, tasks }, "active");
+  }
+}
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { handleFormSubmit } from "./components/scripts/tasks.js";
 
 import { loadLocalStorage } from "./components/scripts/storage.js";
 
+import { getProgress } from "./components/scripts/filter.js";
+
 document.getElementById("themeToggle")?.addEventListener("click", () => {
   document.documentElement.classList.toggle("theme-dark");
   document.documentElement.classList.toggle("theme-light");
@@ -18,9 +20,13 @@ const app = {
   tasks: [],
   template: document.querySelector(".template"),
   tasksList: document.querySelector(".todo_tasks_list"),
-  tasksListElement: document.querySelector(".todo_task_element"),
+  tasksListElement: document.querySelectorAll(".todo_task_element"),
   form: document.querySelector(".form"),
+  activeTasks: document.querySelector(".todo_progress_text--active"),
+  completedTasks: document.querySelector(".todo_progress_text--completed"),
 };
+
+const progressList = document.querySelectorAll(".todo_progress_text");
 
 app.tasksList.addEventListener("change", (evt) => toggleCheckbox(evt, app));
 app.tasksList.addEventListener("click", (evt) => deleteTask(evt, app));
@@ -28,3 +34,9 @@ app.tasksList.addEventListener("click", (evt) => editTask(evt, app));
 
 handleFormSubmit(app);
 loadLocalStorage(app);
+
+progressList.forEach((el) => {
+  el.addEventListener("click", (evt) => {
+    getProgress(evt, app);
+  });
+});
